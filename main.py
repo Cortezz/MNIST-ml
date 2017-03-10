75c71d870f2ee2d5840c45a89ca8902a268819c6
Cortezz <jcortez@Joses-MacBook-Pro.local>
2017-03-10 15:40:59 +0000

    Adds cross-entropy function

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from tensorflow.examples.tutorials.mnist import input_data
-
+import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-x = tf.placeholder(tf.float32,[None, 784])
 
+# Inputs
+x = tf.placeholder(tf.float32,[None, 784])
+# Weights
 W = tf.Variable(tf.zeros([784,10]))
+# Bias
 b = tf.Variable(tf.zeros([10]))
 
+#Softwmax of the output
 y = tf.nn.softmax(tf.matmul(x,W)+b)
+
+
+#Cross-entropy function that minimizes the error
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
