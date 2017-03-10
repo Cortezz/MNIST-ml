f593af87543d2461c5d114412ca84f796e12df10
Cortezz <jospcortez@gmail.com>
2017-03-10 16:01:47 +0000

    Traines the model and checks what the accuracy is

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ y = tf.nn.softmax(tf.matmul(x,W)+b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+session = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+#training the model
+for _ in range(1000):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	session.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))	
+
+
+print(session.run(accuracy, feed_dict={x: mnist.test.images, y_:mnist.test.labels}))
